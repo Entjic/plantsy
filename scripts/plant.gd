c4388d675f1569d0e73e54117be6f1e42f40d6db
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-05-17 18:18:07 +0200

    print plant properties rounded

--- scripts/plant.gd
+++ scripts/plant.gd
@@ -15,5 +15,8 @@ func _on_timer_timeout() -> void:
 	state.tick()
 	health.tick()
 	# todo: reduce health if plant is unhappy
-	print(self.name + ", water: " + str(self.water_level.value)
-	+ " fertilizer: " + str(self.fertilizer_level.value) + " -> state: " + str(self.state.value) + " => ♥: " + str(self.health.value))
+	print(self.name
+	+ ", water: " + str(Util.round_with_decimals(self.water_level.value, 2))
+	+ " fertilizer: " + str(Util.round_with_decimals(self.fertilizer_level.value, 2))
+	+ " -> state: " + str(Util.round_with_decimals(self.state.value, 2))
+	+ " => ♥: " + str(Util.round_with_decimals(self.health.value, 2)))
