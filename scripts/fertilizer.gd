9b9c593f4f2ad03a907cb687175f763d2ec6b995
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-05-17 15:52:15 +0200

    fix naming in fertilizer.gd

--- scripts/fertilizer.gd
+++ scripts/fertilizer.gd
@@ -1,11 +1,11 @@
 extends Holdable
 
-@onready var water_area: Area2D = $Area2D  # Assuming you'll add an Area2D as a child
+@onready var fertilizer_area: Area2D = $Area2D  # Assuming you'll add an Area2D as a child
 
 func _ready():
 	item_type = "fertilizer"
 
 func use(node: Node) -> void:
-	for body in water_area.get_overlapping_bodies():
-		if "water_level" in body.get_property_list().map(func(p): return p.name):
-			body.water_level.value += 0.04
+	for body in fertilizer_area.get_overlapping_bodies():
+		if "fertilizer_level" in body.get_property_list().map(func(p): return p.name):
+			body.fertilizer_level.value += 0.04
