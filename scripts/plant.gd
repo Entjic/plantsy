847d3c664205515d055223f40b251cc67fa43fad
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-05-17 04:38:09 +0200

    use plant Timer as node

--- scripts/plant.gd
+++ scripts/plant.gd
@@ -7,19 +7,7 @@ var health: float = 1.0
 func _ready():
 	add_to_group("plants")
 	item_type = "flower"
-	var timer := Timer.new()
-	add_child(timer)
-	timer.wait_time = 1.0
-	timer.one_shot = false
-	timer.start()
-	timer.timeout.connect(_tick)
-
-func _tick():
-	self.water_level -= (1.0 / 40.0) * log(self.water_level + 1)
-	self.water_level = clamp(self.water_level, 0, MAX_WATER_LEVEL)
-
-	# todo: reduce health if plant is unhappy
-	print(self.name + ", waterlevel: " + str(self.water_level))
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -34,3 +22,11 @@ func increase_water(amount: float) -> void:
 	self.water_level += amount
 	self.water_level = clamp(self.water_level, 0, MAX_WATER_LEVEL)
 	print("Water level: ", self.water_level)
+
+
+func _on_timer_timeout() -> void:
+	self.water_level -= (1.0 / 40.0) * log(self.water_level + 1)
+	self.water_level = clamp(self.water_level, 0, MAX_WATER_LEVEL)
+
+	# todo: reduce health if plant is unhappy
+	print(self.name + ", waterlevel: " + str(self.water_level))
