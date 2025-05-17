e08573078effaea62c06f5fa622799463632773b
Carmen-Kissel <carmenkissel01@gmail.com>
2025-05-17 21:13:03 +0200

    plant types

--- scripts/plant.gd
+++ scripts/plant.gd
@@ -4,10 +4,15 @@ var water_level: WaterLevel = WaterLevel.new()
 var fertilizer_level: FertilizerLevel = FertilizerLevel.new()
 var state: PlantState = PlantState.new([self.water_level, self.fertilizer_level])
 var health: PlantHealth = PlantHealth.new(self.state)
+var type:= "sunflower"
+
+@onready var PlantBottom = $PlantBottom;
+@onready var PlantTop = $PlantTop;
+@onready var PlantPot = $PlantPot;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	item_type = "plant"
+	setTextures()
 
 func _on_timer_timeout() -> void:
 	water_level.tick()
@@ -20,3 +25,16 @@ func _on_timer_timeout() -> void:
 	+ " fertilizer: " + str(Util.round_with_decimals(self.fertilizer_level.value, 2))
 	+ " -> state: " + str(Util.round_with_decimals(self.state.value, 2))
 	+ " => ♥: " + str(Util.round_with_decimals(self.health.value, 2)))
+
+func setTextures():
+	if(type == 'cactus'):
+		$PlantTop.play('plant_1')
+		$PlantBottom.play('plant_1')
+		$PlantPot.play('pink')
+		
+	elif(type == 'sunflower'):
+		$PlantTop.play('plant_2')
+		$PlantBottom.play('plant_2')
+		$PlantPot.play('pink')
+	
+	
--- scripts/shop.gd
+++ scripts/shop.gd
@@ -2,35 +2,16 @@ class_name shop
 extends Node2D
 
 @export var itemType: String;
-#@export var item: Holdable;
 @export var item: PackedScene;
 @export var price: float;
-
-#func reciveItem() :#-> Holdable:
-	#print(itemType + " gekauft");
-	##return item;
 
 func reciveItem() -> Holdable:
 	print(itemType + " gekauft")
 	if item:
-		
 		var new_item = item.instantiate() as Holdable
-		
-		#get_tree().current_scene.add_child(new_item)
-
-		# Setze Position (hier einfach Beispielposition – besser: Spielerposition)
-		#new_item.position = self.position #+ Vector2(32, 0)
-
+		new_item.type = itemType
+		new_item.setTextures()
 		return new_item
-		
 	else:
 		print("Fehler: Kein Item definiert")
 		return null
-
-
-#func can_accept(holdable: Holdable) -> bool:
-	#print("checkin if " + holdable.item_type + " is in " + str(accepted_holdables))
-	#return holdable.item_type in accepted_holdables
-#
-#func center(holdable: Holdable) -> void:
-	#holdable.global_position = global_position
