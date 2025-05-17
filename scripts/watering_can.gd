1c3d039a492390d78b23482645454b5f4da53844
paul <paul@franos.ch>
2025-05-17 15:45:58 +0200

    watering can animations

--- scripts/watering_can.gd
+++ scripts/watering_can.gd
@@ -5,7 +5,10 @@ extends Holdable
 func _ready():
 	item_type = "watering_can"
 
-func use(node: Node) -> void:
+func use(_node: Node) -> void:
 	for body in water_area.get_overlapping_bodies():
 		if "water_level" in body.get_property_list().map(func(p): return p.name):
+			var anim_player := $AnimatedSprite2D
+			anim_player.play("water")
 			body.water_level.value += 0.04
+			break;
