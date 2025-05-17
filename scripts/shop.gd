2d1506fd606ee4953212298a15ef2d7607df53cd
Carmen-Kissel <carmenkissel01@gmail.com>
2025-05-17 22:03:32 +0200

    New Shops

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -1,9 +1,28 @@
-class_name shop
-extends Node2D
+extends StaticBody2D
 
 @export var itemType: String;
 @export var item: PackedScene;
 @export var price: float;
+
+@onready var shopTexture = $shopTexture;
+
+func _ready():
+	price = 10.0
+	setTexture()
+
+func setTexture():
+	if(itemType == "cactus"):
+		$shopTexture.play("cactus")
+	if(itemType == "orange"):
+		$shopTexture.play("orange")
+	if(itemType == "purple"):
+		$shopTexture.play("purple")
+	if(itemType == "red"):
+		$shopTexture.play("red")
+	if(itemType == "rose"):
+		$shopTexture.play("rose")
+	if(itemType == "sunflower"):
+		$shopTexture.play("sunflower")
 
 func reciveItem() -> Holdable:
 	print(itemType + " gekauft")
