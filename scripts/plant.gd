7b8eaeadfb92e741984eaa56320d185fd3354a2f
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-05-18 00:44:06 +0200

    add plant money value

--- scripts/plant.gd
+++ scripts/plant.gd
@@ -5,6 +5,7 @@ var fertilizer_level: FertilizerLevel = FertilizerLevel.new()
 var pesticide_level: PestecideLevel = PestecideLevel.new()
 var state: PlantState = PlantState.new([self.water_level, self.fertilizer_level])
 var health: PlantHealth = PlantHealth.new(self.state)
+var worth: PlantWorth = PlantWorth.new(self.health)
 var type:= "sunflower"
 
 @onready var PlantTexture = $PlantTexture;
@@ -25,13 +26,16 @@ func _on_timer_timeout() -> void:
 	pesticide_level.tick()
 	state.tick()
 	health.tick()
+	worth.tick()
 	# todo: reduce health if plant is unhappy
 	print(self.name
 	+ ", water: " + str(Util.round_with_decimals(self.water_level.value, 2))
 	+ " fertilizer: " + str(Util.round_with_decimals(self.fertilizer_level.value, 2))
 	+ " pestecide: " + str(Util.round_with_decimals(self.pesticide_level.value, 2))
 	+ " -> state: " + str(Util.round_with_decimals(self.state.value, 2))
-	+ " => ♥: " + str(Util.round_with_decimals(self.health.value, 2)))
+	+ " => ♥: " + str(Util.round_with_decimals(self.health.value, 2))
+	+ " ($: " + str(Util.round_with_decimals(self.worth.value, 2)) + ")")
+
 	
 	var state_transformed = self.state.value - 75.0
 	var state_text = str(Util.round_with_decimals(state_transformed, 2))
